96c272dcbb28f2d5a16990a5ebf2a79fa44313cd
Grant Erickson <erick205@umn.edu>
2023-10-18 19:41:00 -0700

    Return the '<null>' string if the service is null.
    This returns the '<null>' string from 'connman_service_get_identifer' if
    the service is null. Unfortunately, not all C Standard Libraries on all
    platforms cope well with a literal null and the '%s' format specifier.

--- src/service.c
+++ src/service.c
@@ -7296,7 +7296,7 @@ struct connman_service *__connman_service_lookup_from_index(int index)
 
 const char *connman_service_get_identifier(struct connman_service *service)
 {
-	return service ? service->identifier : NULL;
+	return service ? service->identifier : "<null>";
 }
 
 const char *__connman_service_get_path(struct connman_service *service)
